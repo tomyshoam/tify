19e74c7d6163f7d05b758274e6a1a71971615786
tomyshoam <tomyshoam@hotmail.com>
2020-08-03 20:03:04 +0300

    test

--- controllers/slugController.js
+++ controllers/slugController.js
@@ -5,7 +5,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.redirectToSlug = catchAsync(async (req, res, next) => {
-  //Get ip of request maybe store in db
+  //Get ip of request maybe store in DB
   const requestIp = req.connection.remoteAddress.split(':')[
     req.connection.remoteAddress.split(':').length - 1
   ];
